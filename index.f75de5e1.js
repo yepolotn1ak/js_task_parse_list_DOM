88038b6343e97cc5b61bb1b27a4ce3823bedab45
Yevhenii <polotnyak2004@gmail.com>
2024-06-04 14:55:04 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const list = [
+    ...document.querySelectorAll("li")
+];
+const newlist = document.querySelector("ul");
+const getEmployees = (listEmployees)=>listEmployees.map((person)=>({
+            name: person.innerText,
+            position: person.dataset.position,
+            salary: +person.dataset.salary.slice(1).split(",").join(""),
+            age: +person.dataset.age
+        }));
+const createSortList = (listEmployees)=>[
+        ...listEmployees
+    ].sort((a, b)=>a.dataset.salary.slice(1).split(",").join("") - b.dataset.salary.slice(1).split(",").join("")).forEach((person)=>newlist.insertAdjacentElement("afterbegin", person));
+getEmployees(list);
+createSortList(list);
+
+//# sourceMappingURL=index.f75de5e1.js.map
